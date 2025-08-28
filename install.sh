2f4d5e7635240795bee31a4a583553ade17484f3
Manfred N <114916491+mntma1@users.noreply.github.com>
2025-08-28 06:29:08 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,12 +1,22 @@
 #!/usr/bin/env bash
 # Created by Manfred - 28.08.2025 05:28 Uhr
 
-read -p 'Das Upload Verzeichnis bitte: ' UPLV 
+read -p 'Das Arbeiits Verzeichnis bitte: ' WDIR 
 #sudo mkdir $UDIR; sudo chown $USER: $UDIR;
-
+#cp .env docker-compose.yml install.sh $WDIR
 cat<<ende
 
-Das Upload Verzeichnis ist: $UPLV
+Das Das Arbeits Verzeichnis ist: $WDIR
+
+cd $WDIR
+
+In der .env  
+  UPLOAD_LOCATION=
+  DB_PASSWORD=
+anpassen
+
+=> nano .env
+=> docker compose up -d
 
 ende
 
