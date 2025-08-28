3cd311a3e1f229c09a8ac8906c38ac2c947a7868
Manfred N <114916491+mntma1@users.noreply.github.com>
2025-08-28 06:57:44 +0200

    von Manfred

--- install.sh
+++ install.sh
@@ -1,22 +1,31 @@
 #!/usr/bin/env bash
 # Created by Manfred - 28.08.2025 05:28 Uhr
-
+clear
 read -p 'Das Arbeiits Verzeichnis bitte: ' WDIR 
 #sudo mkdir $UDIR; sudo chown $USER: $UDIR;
-#cp .env docker-compose.yml install.sh $WDIR
+#cp -fv .env docker-compose.yml install.sh $WDIR
+clear
+
 cat<<ende
+  Das Arbeits Verzeichnis ist: $WDIR
 
-Das Das Arbeits Verzeichnis ist: $WDIR
+   In der Datei: $WDIR/.env
 
-cd $WDIR
+      UPLOAD_LOCATION=
 
-In der .env  
-  UPLOAD_LOCATION=
-  DB_PASSWORD=
-anpassen
+      und
 
-=> nano .env
-=> docker compose up -d
+      DB_PASSWORD=
+
+  anpassen und das Docker-Image bauen.
+    
+    => cd $WDIR
+
+    => nano $WDIR/.env
+
+    => docker compose up -d
+
+ ........Viel Spaß :)
 
 ende
 
