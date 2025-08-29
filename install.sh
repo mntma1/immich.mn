592007f5b85930c9f9251b99101e842efaab08da
Manfred N <114916491+mntma1@users.noreply.github.com>
2025-08-29 20:21:48 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,17 +2,16 @@
 # Created by Manfred - 29.08.2025 05:28 Uhr
 
 clear
-cat<<anfang
-Hier das lokale Arbeits-Verzeichnis eintragen(Ohne / am Ende!). 
-Zum Beispiel: /opt/immich oder auch ~/immich
-anfang
-echo Hier liegen dann die docker-compose Datein-
-echo
-read -p 'Das Arbeiits-Verzeichnis bitte: ' WDIR
-echo
-echo Hier werden die Media Dateien abgelegt(Hier sollte genung Speicherplatz zur Verfügung stehen!).
+echo Hier liegt dann die docker-compose.yml Datei.
+read -p 'Wo soll das docker-compose.yml hin?: ' WDIR
+echo ""
+echo Hier werden die Media-Dateien abgelegt.
+echo Dort sollte sollte genung Speicherplatz zur Verfügung stehen.
 read -p 'Das Upload-Verzeicnis bitte: ' UPLD
-read -p 'Die Zeit Zone(für Deuschland: Europe/Berlin) bitte:' TZN
+echo ""
+echo Für Deuschland "Europe/Berlin" 
+read -p 'Die Zeit Zone bitte:' TZN
+read -p 'Irgendein Datenbank-Passwort bitte: ' DBPASSWD
 
 #sudo mkdir $WDIR; sudo chown $USER: $WDIR;
 #cp -fv .env example.env docker-compose.yml $WDIR
@@ -24,20 +23,16 @@ cat<<ende
 =================================================================================================
 
 Das docker-compose File liegt in: $WDIR
-Das Media Dateien werden in: $UPLD abgespeichert. 
+Das Media Dateien sind dann in: $UPLD 
 
-$EDIR erstellt
-$UPLD erstellt
+Docher Files in: $WDIR
+Uploads iN: $UPLD
 Die Zeit Zone ist: $TZN
+Das DB-Passwortlautet: $DBPASSWD
+
+!!Das Passwort brauchst du dir nicht zu merken, eendient nur zur sichern Verbindung zur DB
 
 Nun folgende Befehle ausführen:
-
-In der Datei: $WDIR/.env
-
-      UPLOAD_LOCATION=/lokales/Upload/Verzeichnis # ohne / am Ende
-      DB_PASSWORD=einSicherEsPassw0rd # [a-zA-Z, 0-9] ohne Lücken, keine Sonderzechen
-
-anpassen und das Docker-Image bauen.
   
     => cd $WDIR
     => nano $WDIR/.env
