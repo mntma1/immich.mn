cdc777979dbbd37918c45702bcf739162eff40f6
Manfred N <114916491+mntma1@users.noreply.github.com>
2025-08-29 23:08:02 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -7,27 +7,28 @@ read -p 'Wo soll das docker-compose.yml hin?: ' WDIR
 echo ""
 echo Hier werden die Media-Dateien abgelegt.
 echo Dort sollte sollte genung Speicherplatz zur Verfügung stehen.
+echo z.B.: /home/immichUpload
 read -p 'Das Upload-Verzeicnis bitte: ' UPLD
 echo ""
 echo Für Deuschland "Europe/Berlin" 
 read -p 'Die Zeit Zone bitte: ' TZN
 read -p 'Irgendein Datenbank-Passwort bitte: ' DBPASSWD
 
-sudo mkdir $WDIR; sudo chown $USER: $WDIR;
+sudo mkdir -pv $WDIR; sudo chown -v $USER: $WDIR;
 cp -fv example.env docker-compose.yml $WDIR
-sudo mkdir $UPLD; sudo chown $USER: $UPLD;
-touch $WDIR/.env
-
-clear
+sudo mkdir -p $UPLD; sudo chown -v $USER: $UPLD;
 
 cat<<dotenvf >$WDIR/.env
+# You can find documentation for all the supported env variables at https://immich.app/docs/install/environment-variables
 UPLOAD_LOCATION=$UPLD
 DB_DATA_LOCATION=$WDIR/postgres
 TZ=$TZN
 IMMICH_VERSION=releas
-DB_DATABASE_NAME=immich
-DB_USERNAME=postgres
 DB_PASSWORD=$DBPASSWD
+# Ab hier nichts ändern
+#####################################################
+DB_USERNAME=postgres
+DB_DATABASE_NAME=immich
 dotenvf
 
 cat<<ende
