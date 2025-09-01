8c0baa351afd0d3ca39a23f0728f87e8c145be8d
Manfred N <114916491+mntma1@users.noreply.github.com>
2025-09-01 19:15:13 +0200

    pwgen -A -n 12 wieder raus genommen

--- install.sh
+++ install.sh
@@ -2,8 +2,8 @@
 # Created by Manfred - 29.08.2025 05:28 Uhr
 
 clear
-echo Hier liegt dann die docker-compose.yml Datei.
-read -p 'Wo sollen das docker-compose.yml und das .env hin?: ' WDIR
+echo Hier liegt dann die docker-compose.yml Datei. Bei mir ligt das immer unter: /opt/immich-app
+read -p 'Wo soll das docker-compose.yml und das .env hin?: ' WDIR
 echo ""
 echo Hier werden die Media-Dateien abgelegt.
 echo Dort sollte sollte genung Speicherplatz zur Verfügung stehen.
@@ -12,9 +12,12 @@ read -p 'Das Upload-Verzeicnis bitte: ' UPLD
 echo ""
 echo Für Deuschland "Europe/Berlin" 
 read -p 'Die Zeit Zone bitte: ' TZN
-# ---> Das Passwort wird nun mit 'pwgen' erzeugt <---
-#echo Nur`A-Za-z0-9` ohne Lücken oder Sonderzeichen 
-#read -p 'Irgendein Datenbank-Passwort bitte: ' DBPASSWD
+echo ""
+echo Nur [A-Za-z,0-9], Ohne Lücken oder Sonderzeichen
+echo Das lässt sich gut mit: pwgen -A -n 16 erzeugen.
+read -p 'Irgendein Datenbank-Passwort bitte: ' DBPASSWD
+# Oder mit nun mit: => DB_PASSWORD=$(pwgen -A -n 12) <= erzeugen 
+# Dazu mus aber pwgen installiert sein. => sudo  apt install pwgen
 
 sudo mkdir -p $WDIR; sudo chown $USER: $WDIR;
 cp -f example.env docker-compose.yml $WDIR
@@ -38,7 +41,7 @@ IMMICH_VERSION=release
  
 # Connection secret for postgres. You should change it to a random password
 # Please use only the characters `A-Za-z0-9`, without special characters or spaces
-DB_PASSWORD=$(pwgen -A -n 12)
+DB_PASSWORD=$DBPASSWD
  
 # The values below this line do not need to be changed
 ###################################################################################
